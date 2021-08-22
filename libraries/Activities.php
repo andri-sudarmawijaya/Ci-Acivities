9dbc4f96e9aaaf97fa897410e7663e5df05d30b6
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-08-23 00:26:31 +0700

    add telegram

--- libraries/Activities.php
+++ libraries/Activities.php
@@ -2,6 +2,7 @@
 
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
+
 Class Activities{
 
     /**
@@ -12,7 +13,7 @@ Class Activities{
      * @param string $content
      *
      */
-    public function log_activities($param){
+    public function log($param = array()){
 
         $this->CI = & get_instance();  //get instance, access the CI superobject
 
@@ -26,5 +27,34 @@ Class Activities{
         $this->CI->Activities_model->insert($activities);
     }
 
-}
+    /**
+     * Telegram
+     *
+     * @var string $botToken
+     * @var string $chatId
+     * @var string $message
+     *
+     */
+    public function telegram($var = array()){
 
+        /*
+         * defined you telegram bot and chatId here as default if you don want to declare it in your controller.
+         */
+        $botToken = $var['botToken'] ?? "XXXXXXXXXX:YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY";
+        $chatId=$var['chatId'] ?? -ZZZZZZZZZ;  //** ===>>>NOTE: this chatId MUST be the chat_id of a person or group, NOT another bot chatId !!!**
+
+        $website="https://api.telegram.org/bot".$botToken;
+        $params=[
+            'chat_id'=>$chatId,
+            'text'=> $var['content'] ?? 'This is my message !!!',
+        ];
+        $ch = curl_init($website . '/sendMessage');
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close($ch);
+    }
+}
