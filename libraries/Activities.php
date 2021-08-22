0d9b9f0cf678eb7561be788881e0352297cc8433
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-08-23 00:53:41 +0700

    update Actifities

--- libraries/Activities.php
+++ libraries/Activities.php
@@ -41,13 +41,20 @@ Class Activities{
          * defined you telegram bot and chatId here as default if you don want to declare it in your controller.
          */
         $botToken = $var['botToken'] ?? "XXXXXXXXXX:YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY";
-        $chatId=$var['chatId'] ?? -ZZZZZZZZZ;  //** ===>>>NOTE: this chatId MUST be the chat_id of a person or group, NOT another bot chatId !!!**
+        $chatId = $var['chatId'] ?? -ZZZZZZZZZ;  //** ===>>>NOTE: this chatId MUST be the chat_id of a person or group, NOT another bot chatId !!!**
+
 
         $website="https://api.telegram.org/bot".$botToken;
         $params=[
             'chat_id'=>$chatId,
             'text'=> $var['content'] ?? 'This is my message !!!',
         ];
+        echo '<pre>';
+        print_r($var);
+        echo '<br />-<br />-<br />';
+        print_r($params);
+        echo '</pre>';
+
         $ch = curl_init($website . '/sendMessage');
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
